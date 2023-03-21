606b2eb924f1f03dd817a1537fa4a437e98d20b2
SrinathBala <92857200+SrinathBala@users.noreply.github.com>
2023-03-21 19:49:47 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum install python3-pip git -y
-sudo git clone "https://github.com/SrinathBala/helloworld.git"
-sudo pip3 install flask
-cd /flask
-python3 app.py
+sudo yum install python3-pip git docker -y
+sudo git clone "https://github.com/SrinathBala/docker-flask.git"
+cd /docker-test
+sudo docker build -t my-image .
+sudo docker run -p 80:80 my-image
