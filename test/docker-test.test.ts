07f53c0459cfc4bdf3f6cced1507a13291661211
Srinath Bala <srinathbala00@gmail.com>
2023-03-21 18:39:18 +0530

    Initial commit

---
+++ test/docker-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DockerTest from '../lib/docker-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/docker-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DockerTest.DockerTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
